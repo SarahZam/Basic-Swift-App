9fe8707a400e82188528a65a55b57ccc302e0869
Sarah Zaman <szaman@u.rochester.edu>
2020-09-26 20:46:10 -0400

    "main board"

--- Colorgame/ViewController.swift
+++ Colorgame/ViewController.swift
@@ -9,11 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Color1: UIView!
+    @IBOutlet weak var Color2: UIView!
+    @IBOutlet weak var Color4: UIView!
+    @IBOutlet weak var Color3: UIView!
+    
+    @IBOutlet weak var colorName: UILabel!
+    @IBOutlet weak var scoreValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //right1
+       let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(handleTap1))
+       Color1.addGestureRecognizer(tapGesture1)
+        
+        //left1
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(handleTap2))
+        Color2.addGestureRecognizer(tapGesture2)
+        
+        //right1
+        let tapGesture3 = UITapGestureRecognizer(target: self, action: #selector(handleTap3))
+              Color3.addGestureRecognizer(tapGesture3)
+               
+        //left1
+        let tapGesture4 = UITapGestureRecognizer(target: self, action:
+            #selector(handleTap4))
+            Color4.addGestureRecognizer(tapGesture4)
+       
+    
     }
+    
+    @IBAction func onScreenTap(_ sender: Any) {
+        
+         if view.backgroundColor == UIColor.blue {
+                   view.backgroundColor = UIColor.red
+               } else {
+                   view.backgroundColor = UIColor.blue
+               }
+    }
+    
+    @objc func handleTap1() {
+        
+        // example task: change background color
+        if Color1.backgroundColor == UIColor.blue {
+            Color1.backgroundColor = UIColor.red
+        } else {
+            Color1.backgroundColor = UIColor.blue
+        }
+        
+    }
+    
+    @objc func handleTap2() {
+          
+          // example task: change background color
+          if Color2.backgroundColor == UIColor.blue {
+              Color2.backgroundColor = UIColor.red
+          } else {
+              Color2.backgroundColor = UIColor.blue
+          }
+          
+      }
+    
+    @objc func handleTap3() {
+           
+           // example task: change background color
+           if Color3.backgroundColor == UIColor.blue {
+               Color3.backgroundColor = UIColor.red
+           } else {
+               Color3.backgroundColor = UIColor.blue
+           }
+           
+       }
+       
+       @objc func handleTap4() {
+             
+             // example task: change background color
+             if Color4.backgroundColor == UIColor.blue {
+                 Color4.backgroundColor = UIColor.red
+             } else {
+                 Color4.backgroundColor = UIColor.blue
+             }
+             
+         }
 
 
 }
